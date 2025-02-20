7ae606c059c3697e9489dc24fd8e0a3cf69b4efc
Anshay-Paying <anshay.paying@gmail.com>
2025-02-20 14:38:00 +0530

    Added test case for empty string returning 0

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -2,7 +2,10 @@ import 'package:string_calculator_tdd/string_calculator_tdd.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  group('String Calculator', () {
+    test('returns 0 for an empty string', () {
+      final calculator = StringCalculator();
+      expect(calculator.add(''), equals(0));
+    });
   });
 }
